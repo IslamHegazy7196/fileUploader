1f78044973f723afd77e3b2d92f0992478d225b0
IslamHegazy7196 <islamfathihegazy04@gmai.com>
2022-04-28 00:04:50 +0200

    upload

--- app.js
+++ app.js
@@ -3,18 +3,24 @@ require('express-async-errors');
 
 const express = require('express');
 const app = express();
+const fileUpload=require('express-fileupload')
 
 // database
 const connectDB = require('./db/connect');
-
+// routes
+const productRoutes=require('./routes/productRoutes')
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.use(express.static('./public'))
+app.use(express.json())
+app.use(fileUpload())
 app.get('/', (req, res) => {
   res.send('<h1>File Upload Starter</h1>');
 });
 
+app.use('/api/v1/products',productRoutes)
 // middleware
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
